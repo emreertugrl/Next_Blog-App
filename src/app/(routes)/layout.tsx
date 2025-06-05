cfa17edf17ae88d0fa6708288b2202494564f50e
emreertugrl <emrertugrl@gmail.com>
2025-06-05 17:02:50 +0300

    header,footer,home page defined

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import Header from "../../components/layout/header/Header";
+import Footer from "../../components/layout/footer/Footer";
 interface RoutesLayoutProps {
   children: React.ReactNode;
 }
 
 const RoutesLayout = ({ children }: RoutesLayoutProps) => {
   return (
-    <div>
-      Header
-      {children}
-      Footer
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow container mx-auto py-4">{children}</main>
+      <Footer />
     </div>
   );
 };
