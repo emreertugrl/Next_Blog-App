b17b631f478312637fe5ded232f7f5b47b2f3b54
emreertugrl <emrertugrl@gmail.com>
2025-06-05 20:47:59 +0300

    Image and metadata defined

--- src/app/(routes)/(home)/page.tsx
+++ src/app/(routes)/(home)/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <img src="/1.png" alt="resim" />
+      <Image src="/1.png" width={1170} height={500} alt="resim" />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/lib/siteMetadata.ts
@@ -0,0 +1,19 @@
+export const siteMetadata = {
+  title: "Next.js Blog Marketplace Optimized Seo ",
+  author: "Emre Ertuğrul",
+  headerTitle: "Next.js Blog",
+  description: "A blog created with Next.js, Tailwind.css .",
+  language: "en-us",
+  theme: "system", // system, dark or light varsayılan
+  siteUrl: "https://www.myurl.com", // your website URL
+  siteLogo: "/file.svg",
+  socialBanner: "/1.png", // add social banner in the public folder
+  email: "mygmail@gmail.com",
+  github: "https://github.com/mygithub",
+  twitter: "https://twitter.com/",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com/@myyoutube",
+  linkedin: "https://www.linkedin.com/in/my-linkedin",
+  dribbble: "https://www.dribbble.com",
+  locale: "en-US",
+};
