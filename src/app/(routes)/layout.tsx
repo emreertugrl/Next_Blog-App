ec89dbe98c4a8848dd08159d362385be0e84aed0
emreertugrl <emrertugrl@gmail.com>
2025-06-05 16:46:47 +0300

    routes group have special layout

---
+++ src/app/(routes)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+interface RoutesLayoutProps {
+  children: React.ReactNode;
+}
+
+const RoutesLayout = ({ children }: RoutesLayoutProps) => {
+  return (
+    <div>
+      Header
+      {children}
+      Footer
+    </div>
+  );
+};
+
+export default RoutesLayout;
